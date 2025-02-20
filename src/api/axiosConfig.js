6bbf7611db4c2604911aebee9a172a6d9ec4015b
preshita zankat <94054515+preshi-zankat@users.noreply.github.com>
2025-02-20 11:34:42 +0530

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "https://blog-backend-3l4r.onrender.com/api",
   timeout: 5000,
-  withCredentials: true, // Ensure credentials are sent if needed
+  //withCredentials: true, // Ensure credentials are sent if needed
   headers: {
     "Content-Type": "application/json",
   },
